94fba48f92a811cf02a2085b324484177cc94751
kaienlin <kaienlin1999@gmail.com>
2019-06-23 15:55:13 +0800

    Fix bug

--- Model/GameObject/player.py
+++ Model/GameObject/player.py
@@ -35,7 +35,7 @@ class Player(object):
     def store_price(self, bases):
         if self.position[0] <= bases[self.index].center[0] + bases[self.index].length/2 \
             and self.position[0] >= bases[self.index].center[0] - bases[self.index].length/2 \
-            or self.position[1] <= bases[self.index].center[1] + bases[self.index].length/2 \
+            and self.position[1] <= bases[self.index].center[1] + bases[self.index].length/2 \
             and self.position[1] >= bases[self.index].center[1] - bases[self.index].length/2:
             bases[self.index].change_value_sum(self.price)
             self.price = 0
