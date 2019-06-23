69212a45addbd8b0486038632063eb1fda97faf8
Wei-Han Chen <yp155136@gmail.com>
2019-06-23 10:50:33 +0800

    Fix bugs

--- Model/const.py
+++ Model/const.py
@@ -1,10 +1,24 @@
 #dir const
-dir_const = [
-	[0,0],	#steady
-	[0,-1],	#down
-	[0,1],	#up
-	[1,0],	#right
-	[-1,0],	#left
+"""
+DIR_U  = 1
+DIR_RU = 2
+DIR_R  = 3
+DIR_RD = 4
+DIR_D  = 5
+DIR_LD = 6
+DIR_L  = 7
+DIR_LU = 8
+"""
+dir_mapping = [
+	[0, 0],	         #steady
+	[0, 1],	         #up
+	[0.707, 0.707],  #up right
+    [1, 0],	         #right
+    [0.707, -0.707], #right down
+	[0, -1],	     #down
+    [-0.707, -0.707],#left down
+	[-1, 0],	     #left
+    [-0.707, 0.707], #left up
 ]
 
 # player
