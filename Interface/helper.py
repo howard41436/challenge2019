7fc19a623d05c08ac0353f67868213863286f6af
edisonhello <edisonhello@hotmail.com>
2019-06-24 16:32:50 +0800

    Set player_id default being self.id

--- Interface/helper.py
+++ Interface/helper.py
@@ -9,7 +9,7 @@ import View.const as view_const
 class Helper(object):
     def __init__(self, model, index):
         self.model = model
-        self.index = index
+        self.player_id = index
         self.oil_radius = model_const.oil_radius
         self.player_radius = model_const.player_radius
         self.bag_capacity = model_const.bag_capacity
@@ -19,34 +19,43 @@ class Helper(object):
         
 
     # Get player data
+    def get_self_id(self):
+        return self.player_id
+
     def get_players_position(self):
         return [tuple(player.position) for player in self.model.player_list]
-    def get_player_position(self, player_id):
+    def get_player_position(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         return tuple(self.model.player_list[player_id].position)
 
     def get_players_direction(self):
         return [tuple(player.direction) for player in self.model.player_list]
-    def get_player_direction(self, player_id):
+    def get_player_direction(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         return tuple(self.model.player_list[player_id].direction)
 
     def get_players_value(self):
         return [player.value for player in self.model.player_list]
-    def get_player_value(self, player_id):
+    def get_player_value(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         return self.model.player_list[player_id].value
 
     def get_players_bag(self):
         return [player.bag for player in self.model.player_list]
-    def get_player_bag(self, player_id):
+    def get_player_bag(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         return self.model.player_list[player_id].bag
 
     def get_players_is_AI(self):
         return [player.is_AI for player in self.model.player_list]
-    def get_player_is_AI(self, player_id):
+    def get_player_is_AI(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         return self.model.player_list[player_id].is_AI
 
     def get_players_speed(self):
         return [player.speed for player in self.model.player_list]
-    def get_player_speed(self, player_id):
+    def get_player_speed(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         return self.model.player_list[player_id].speed
 
     # Get pet data
@@ -58,7 +67,8 @@ class Helper(object):
     # Get base data
     def get_bases_center(self):
         return [tuple(base.center) for base in self.model.base_list]
-    def get_base_center(self, player_id):
+    def get_base_center(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         return tuple(self.model.base_list[player_id])
         
     # Get game informations
@@ -66,12 +76,14 @@ class Helper(object):
         return self.model.timer
 
     # Extra functions
-    def get_nearest_player(self, player_id):
+    def get_nearest_player(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         my_pos = self.get_player_position(player_id)
         players = self.get_players_position()
         return min(players.remove(my_pos), key=lambda player: (player - my_pos).magnitude())
 
-    def get_nearest_oil(self, player_id):
+    def get_nearest_oil(self, player_id = None):
+        if player_id == None: player_id = self.player_id
         my_pos = self.get_player_position(player_id)
         oils = self.get_oils()
         return min(oils, key=lambda oil: (oil - my_pos).magnitude())
