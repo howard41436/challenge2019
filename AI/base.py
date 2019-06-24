fbf13a259d51a8a1268d95b0fe62083c0ef08ad4
anderson <b07902060@ntu.edu.tw>
2019-06-24 17:21:54 +0800

    my_AI

--- AI/base.py
+++ AI/base.py
@@ -1,14 +1,18 @@
-DIR_stop = 0
-DIR_U    = 1
-DIR_RU   = 2
-DIR_R    = 3
-DIR_RD   = 4
-DIR_D    = 5
-DIR_LD   = 6
-DIR_L    = 7
-DIR_LU   = 8
-
-
+"""
+const of AI code use.
+"""
+AI_DIR = [0, 1 ,2 ,3 ,4 ,5 ,6, 7, 8]
+AI_dir_mapping = [
+    [0, 0],             #steady
+    [0, -1],             #up
+    [0.707, -0.707],     #up right
+    [1, 0],             #right
+    [0.707, 0.707],    #right down
+    [0, 1],            #down
+    [-0.707, 0.707],   #left down
+    [-1, 0],	        #left
+    [-0.707, -0.707],    #left up
+]
 """
 a base of AI.
 """
@@ -17,5 +21,5 @@ class BaseAI:
         self.skill = []
         self.helper = helper
 
-    def decide( self ):
-        pass
+    def decide(self):
+    	pass
--- AI/team_default.py
+++ AI/team_default.py
@@ -2,48 +2,39 @@ from AI.base import *
 
 from pygame.math import Vector2 as Vec
 import random
+import math
 
 class TeamAI(BaseAI):
     def __init__(self, helper):
         self.helper = helper
         self.skill = []
-
         self.last_dir = random.randint(1, 8)
 
-    def get_best_oil_position(self):
-        my_pos = self.helper.get_player_position(3)
-        oils = self.helper.get_oils_item()
+    def get_best_oil_vec(self):
+        my_pos = self.helper.get_player_position(self.helper.index)
+        oils = self.helper.get_all_oil_data()
         best_pos = None
         best_cp = -1
-        for oil in oils:
-            cp = oil.price / (Vec(my_pos) - Vec(oil.position)).length()
+        for pos, price in oils:
+            cp = price / ((my_pos - pos).length() ** (3/2)) 
             if cp > best_cp:
                 cp = best_cp
-                best_pos = oil.position
-        return best_pos, my_pos
+                best_pos = pos
+        return (best_pos - my_pos)
+
+    def get_best_player_to_comminism(self):
+        myself = self.helper.get_my_data()
+        other = self.helper.get_all_player_data()
+        best_pos = None
+        best_cp = -1
 
     def decide(self):
-        radius = self.helper.get_player_radius()
-        carry = self.helper.get_player_value(3)
-        best_pos, my_pos = self.get_best_oil_position()
-        home = self.helper.get_base_center(3)
-        if carry > 2000:
-            if home[1] - my_pos[1] < -radius: return DIR_U
-            if home[0] - my_pos[0] > radius: return DIR_R
-            if home[0] - my_pos[0] < -radius: return DIR_L
-            if home[1] - my_pos[1] > radius: return DIR_D
-        if best_pos[1] - my_pos[1] < -radius: return DIR_U
-        if best_pos[0] - my_pos[0] > radius: return DIR_R
-        if best_pos[0] - my_pos[0] < -radius: return DIR_L
-        if best_pos[1] - my_pos[1] > radius: return DIR_D
-"""
-DIR_stop = 0
-DIR_U    = 1
-DIR_RU   = 2
-DIR_R    = 3
-DIR_RD   = 4
-DIR_D    = 5
-DIR_LD   = 6
-DIR_L    = 7
-DIR_LU   = 8
-"""
+        AI_move_dir = 0
+        vec_dot = 0
+        best_oil_vec = Vec( self.get_best_oil_vec() )
+        for dir_vec in AI_dir_mapping:
+            if Vec(dir_vec).dot(best_oil_vec) > vec_dot:
+                vec_dot = Vec(dir_vec).dot(best_oil_vec)
+                AI_move_dir = AI_dir_mapping.index(dir_vec)
+        return AI_move_dir
+
