e256878ac132456d6931356cbd3be6e1e7883948
jay-in-git <moom60225@gmail.com>
2019-06-25 16:29:49 +0800

    Smoothe the motion

--- AI/team_jay.py
+++ AI/team_jay.py
@@ -8,7 +8,7 @@ class TeamAI(BaseAI):
         self.helper = helper
         self.skill = []
 
-        self.last_dir = random.randint(1, 8)
+        self.last_dir = random.randint(1, 😎
 
     def get_best_oil_position(self):
         my_pos = self.helper.get_player_position()
@@ -22,24 +22,39 @@ class TeamAI(BaseAI):
                 best_cp = cp
                 best_pos = oil_poses[i]
         return best_pos, my_pos, best_cp
-
+    def get_dir(self, dest, my_pos):
+        direct = [
+        [0, 0],             #steady
+        [0, -1],             #up
+        [0.707, -0.707],     #up right
+        [1, 0],             #right
+        [0.707, 0.707],    #right down
+        [0, 1],            #down
+        [-0.707, 0.707],   #left down
+        [-1, 0],            #left
+        [-0.707, -0.707],    #left up
+        ]
+        new = Vec(dest) - Vec(my_pos)
+        maximum = 0
+        record = 0
+        for i in range(9):
+            if maximum < (new).dot(Vec(direct[i])):
+                maximum = (new).dot(Vec(direct[i]))
+                record = i
+        return record
     def decide(self):
         radius = self.helper.player_radius
         carry = self.helper.get_player_value()
         best_pos, my_pos, best_cp = self.get_best_oil_position()
         home = self.helper.get_base_center()
         dest = best_pos
-        home_cp = carry / ((Vec(my_pos) - Vec(home)).length()**2 + 1)
+        home_cp = 1e-5 * carry if self.helper.get_distance(self.helper.get_base_center(), my_pos) \
+                     <= self.helper.get_distance_to_center(self.helper.get_base_center()) \
+                     else 3e-8 * carry * self.helper.get_distance(self.helper.get_base_center(), my_pos)
+        print(home_cp, best_cp)
         if home_cp > best_cp:
             dest = home
-        togo = [DIR_U, DIR_RU, DIR_LU] if dest[1] - my_pos[1] < -radius else [DIR_D, DIR_RD, DIR_LD]
-        if dest[0] - my_pos[0] > radius:
-            togo = togo[1]
-        elif dest[0] - my_pos[0] < -radius:
-            togo = togo[2]
-        else:
-            togo = togo[0]
-        return togo
+        return self.get_dir(dest, my_pos)
 """
 DIR_stop = 0
 DIR_U    = 1
