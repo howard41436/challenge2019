0534c1b169ac9910d753c3e6ea6ed12962a70c30
kaienlin <kaienlin1999@gmail.com>
2019-06-25 12:12:00 +0800

    Fix bugs

--- Model/GameObject/market.py
+++ Model/GameObject/market.py
@@ -12,7 +12,7 @@ class Market(object):
         self.item = None
 
     def generate_item(self, player_list, oil_list, base_list, player_index):
-        self.item = random.choice(self.item_list)()
+        self.item = random.choice(self.item_list)(player_list, oil_list, base_list, player_index)
         # self.item = Item.MagnetAttract(player_list, oil_list, base_list, player_index)
 
     def sell(self):
