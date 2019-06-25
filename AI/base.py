ec9ecdc6d61de18c0694cbe15fabb706e769321c
anderson <b07902060@ntu.edu.tw>
2019-06-25 13:19:46 +0800

    update

--- AI/base.py
+++ AI/base.py
@@ -1,18 +1,7 @@
 """
 const of AI code use.
 """
-AI_DIR = [0, 1 ,2 ,3 ,4 ,5 ,6, 7, 8]
-AI_dir_mapping = [
-    [0, 0],             #steady
-    [0, -1],             #up
-    [0.707, -0.707],     #up right
-    [1, 0],             #right
-    [0.707, 0.707],    #right down
-    [0, 1],            #down
-    [-0.707, 0.707],   #left down
-    [-1, 0],	        #left
-    [-0.707, -0.707],    #left up
-]
+
 """
 a base of AI.
 """
