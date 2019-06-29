e611d62bc4f388cd34dc010f53aa08115b0c1f28
andy94077 <andy94077@gmail.com>
2019-06-30 00:15:00 +0800

    Optimize coding style of team_yee.py

--- AI/team_yee.py
+++ AI/team_yee.py
@@ -7,6 +7,8 @@ from pygame.math import Vector2 as Vec
 import random
 from datetime import datetime
 
+market_pos = Vec(view_const.game_size) / 2
+
 class TeamAI(BaseAI):
     def __init__(self, helper):
         self.helper = helper
@@ -30,7 +32,7 @@ class TeamAI(BaseAI):
         return sd / np.sqrt(2 * np.pi) * (np.exp(-0.5 * low ** 2) - np.exp(-0.5 * high ** 2)) + mu * (norm.cdf(high, mu, sd) - norm.cdf(low, mu, sd))
     
     def get_oil_expected_value(self, pos, level):
-        dist_from_center = (pos - Vec(view_const.game_size)/2).length() - model_const.market_radius
+        dist_from_center = (pos - market_pos).length() - model_const.market_radius
         mu = model_const.curve_a / (dist_from_center + model_const.curve_b)
         sd = model_const.price_scale
         level_range = [model_const.price_min, 400, 600, 800, model_const.price_max]
@@ -50,7 +52,7 @@ class TeamAI(BaseAI):
         best_cp = -1
         for pos, oil_level in oils:
             cp = self.get_oil_expected_value(pos, oil_level) * (1 if (pos - self.home).length() <= self.half_game_size * 1.2 else (1 - 0.5 * (pos - self.home).length() / (self.helper.game_size[0] * 1.414))**2) \
-                 / ((self.pos - pos).length_squared()/self.speed + 1e-5)
+                 / ((self.pos - pos).length_squared()/(self.speed + 1e-5))
             if cp > best_cp:
                 best_cp = cp
                 best_pos = pos
@@ -59,23 +61,17 @@ class TeamAI(BaseAI):
     def get_speed_by_value(self, val):
         return model_const.speed_multiplier ** self.equipments[model_const.speed_up_idx] * max(model_const.player_speed_min, model_const.player_normal_speed - model_const.player_speed_decreasing_rate * val)
 
-    def attack(self):
+    def attack(self, players):
         #return None, -2
-        players = zip(list(map(Vec, self.helper.get_players_position())),
-            [i*j for i,j in zip(self.helper.get_players_speed(), list(map(Vec, self.helper.get_players_direction())))],
-            self.helper.get_players_value())
         best_pos, best_cp = Vec(0, 0), -1
         for i, item in enumerate(players):
             pos, v, val = item
-            #print(type(v), v)
             if i == self.id:
                 continue
             if (val - self.carry) / 2 >= 1000: #rob it if it can gain enough money
                 # it can catch up with the player
-                #to_home_dist = (pos-self.home).length()
                 dist = (self.pos - pos).length()
                 t = dist / (self.speed - (pos - self.pos).normalize().dot(v) + 1e-5)
-                #to_home_t = (self.pos + self.speed * my_dir * t).length() / self.get_speed_by_value((val + self.carry) / 2)
                 player_home_dist = (Vec(self.helper.get_base_center(i)) - pos).length() - self.helper.base_length / 2
                 if player_home_dist / (v.length()+1e-3) > t:
                     cp = (val - self.carry) * (player_home_dist / (dist+1e-5)) / dist / t * (1 if (pos - self.home).length() <= self.half_game_size * 1.2 else (1 - 0.5 * (pos - self.home).length() / (self.helper.game_size[0] * 1.414))**1.5)
@@ -90,14 +86,10 @@ class TeamAI(BaseAI):
             if maximum < vec.dot(v):
                 maximum = vec.dot(v)
                 record = i
-        print(record)
+        #print(record)
         return record
     
-    def be_chased(self):
-        players = zip(list(map(Vec, self.helper.get_players_position())),
-            [i*j for i,j in zip(self.helper.get_players_speed(), list(map(Vec, self.helper.get_players_direction())))],
-            self.helper.get_players_value())
-        
+    def be_chased(self, players):
         thief_pos, loss = Vec(0,0), 0
         for i, item in enumerate(players):
             pos, v, val = item
@@ -110,27 +102,20 @@ class TeamAI(BaseAI):
                     thief_pos, loss = pos, (self.carry - val) / 2
         return thief_pos, loss
 
-    def eat_btw(self, dest, go_home, go_home_now):
+    def eat_btw(self, dest, go_home):
         nearest_oil = self.helper.get_nearest_oil()
         ongoing_vec = dest - self.pos
         if nearest_oil is not None:
             nearest_oil = Vec(nearest_oil)
             nearest_vec = nearest_oil - self.pos
             if nearest_vec.length()/self.speed < 7 and nearest_vec.length()>0: #7 ticks
-                if not go_home and not go_home_now or ongoing_vec.normalize().dot(nearest_vec.normalize()) > 1e-3:
+                if not go_home or ongoing_vec.normalize().dot(nearest_vec.normalize()) > 1e-3:
                     return self.get_dir(nearest_vec)
                 else:
                     return self.get_dir(ongoing_vec)
         return self.get_dir(ongoing_vec)
     
-    def get_home_cp(self, loss):
-        to_home_dist = (self.pos - self.home).length()
-        if self.carry <= 1000 or to_home_dist <= 1e-3:
-            return 0
-        carry_transform = (1 / 160 * (self.carry - 2500))** 3 + 1000
-        players = zip(list(map(Vec, self.helper.get_players_position())),
-            [i*j for i,j in zip(self.helper.get_players_speed(), list(map(Vec, self.helper.get_players_direction())))],
-            self.helper.get_players_value())
+    def get_other_threat(self, players):
         other_threat = 0
         for i, item in enumerate(players):
             pos, v, val = item
@@ -138,7 +123,15 @@ class TeamAI(BaseAI):
                 continue
             if self.carry > val:
                 other_threat += (self.carry - val) / 2 / ((pos - self.pos).length_squared() / (abs(self.speed - v.length()) + 1e-5))
+        return other_threat
+
+    def get_home_cp(self, loss, players):
+        to_home_dist = (self.pos - self.home).length()
+        if self.carry <= 1000 or to_home_dist <= 1e-3:
+            return 0
+        carry_transform = (1 / 160 * (self.carry - 2500))** 3 + 1000
         
+        other_threat = self.get_other_threat(players)
         if to_home_dist > self.half_game_size * 1.4:
             dist_cp = 5e-5 * (to_home_dist-self.half_game_size * 1.4) ** 0.8
         else:
@@ -151,35 +144,43 @@ class TeamAI(BaseAI):
         self.pos = Vec(self.helper.get_player_position())
         self.speed = self.helper.get_player_speed()
         
-        oil_pos, best_cp = self.get_best_oil_cp()
-        attack_pos, attack_cp = self.attack()
-        thief_pos, loss = self.be_chased()
-        home_cp = self.get_home_cp(loss)
-        
+        players = list(zip(list(map(Vec, self.helper.get_players_position())),
+            [i*j for i,j in zip(self.helper.get_players_speed(), list(map(Vec, self.helper.get_players_direction())))],
+            self.helper.get_players_value()))
+
+        oil_pos, oil_cp = self.get_best_oil_cp()
+        attack_pos, attack_cp = self.attack(players)
+        thief_pos, loss = self.be_chased(players)
+        home_cp = self.get_home_cp(loss, players)
+
         #print(best_cp, attack_cp)
-        dest = oil_pos
+        dest, best_cp = oil_pos, oil_cp
         action = 'oil'
         
         if attack_cp > best_cp:
             #print('attack '+str(datetime.now().time())[:8],end=' ')
-            dest = attack_pos
+            dest, best_cp = attack_pos, attack_cp
             action = 'attack'
 
-        go_home = go_home_now = False
+        go_home = go_straight = False
         if home_cp > best_cp or self.carry >= 5000:
-            dest = self.home
+            dest, best_cp = self.home, home_cp
             go_home = True
-            go_home_now = True if loss >= 1000 else False
-            # if go_home_now:
+            go_straight = (loss >= 1000)
+            # if go_straight:
             #     print('go_home_now ' + str(datetime.now().time())[:8], end=' ')
             # else:
             #     print('go home', end=' ')
             action = 'home'
         
-        available_choice = np.array([[oil_pos, best_cp], [self.home,home_cp], [attack_pos, attack_cp]])
-        print(available_choice[:,1].ravel(),end=' ')
-        available_choice = available_choice[np.argsort(available_choice[:,1])[::-1]]
-        return self.eat_btw(dest, go_home, go_home_now) if action != 'attack' else self.get_dir(dest - self.pos)
+        # available_choice = np.array([[oil_pos, best_cp], [self.home,home_cp], [attack_pos, attack_cp]])
+        # print(available_choice[:,1].ravel(),end=' ')
+        # available_choice = available_choice[np.argsort(available_choice[:,1])[::-1]]
+
+        if action == 'attack' or go_straight:
+            return self.get_dir(dest - self.pos)
+        else:
+            return self.eat_btw(dest, go_home)
 """
 DIR_stop = 0
 DIR_U    = 1
