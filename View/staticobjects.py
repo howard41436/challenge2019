427661de7117fdd135155e4366350cb2a17048b6
Kevin Wang <b07902034@ntu.edu.tw>
2019-06-30 17:09:29 +0800

    Add player image

--- View/staticobjects.py
+++ View/staticobjects.py
@@ -97,20 +97,20 @@ class View_characters(__Object_base):
 
 
 class View_players(__Object_base):
-    """images_color = tuple(
+    images_color = tuple(
         view_utils.scaled_surface(
             pg.image.load(os.path.join(view_const.IMAGE_PATH, f'player_color{_rainbow}.png')),
             0.2
         )
         for _rainbow in range(0,19,1)
-    )"""
+    )
     image_freeze = view_utils.scaled_surface(pg.image.load(os.path.join(view_const.IMAGE_PATH, 'freeze.png')),0.5)
-    image = view_utils.scaled_surface(pg.image.load( os.path.join(view_const.IMAGE_PATH, 'koreanfish.png') ), 0.2)
+    image_koreanfish = view_utils.scaled_surface(pg.image.load( os.path.join(view_const.IMAGE_PATH, 'koreanfish.png') ), 0.2)
 
     def __init__(self, model):
         self.model = model
         self.color_switch = [0, 0, 0, 0]
-        """self.images = tuple(
+        self.images = tuple(
             view_utils.scaled_surface(
                 view_utils.replace_color(os.path.join(view_const.IMAGE_PATH,'player_outfit.png'),
                                         view_const.COLOR_WHITE,
@@ -124,16 +124,16 @@ class View_players(__Object_base):
     def init_convert(cls):
         cls.images = tuple( _image.convert_alpha() for _image in cls.images )
         cls.image_freeze = cls.image_freeze.convert_alpha()
-        cls.images_color = tuple( _image.convert_alpha() for _image in cls.images_color)"""
+        cls.images_color = tuple( _image.convert_alpha() for _image in cls.images_color)
 
     def draw(self, screen):
         players = self.model.player_list
         for _i in range(len(players)):
             angle = ((8 - players[_i].direction_no) % 8 - 3) * 45
-            image = pg.transform.rotate(self.image, angle)
-            if not players[_i].is_invincible: image = pg.transform.rotate(self.image, angle)
+            image = pg.transform.rotate(self.images[_i], angle)
+            if not players[_i].is_invincible: image = pg.transform.rotate(self.images[_i], angle)
             else: 
-                """image = pg.transform.rotate(self.images_color[self.color_switch[_i]%19], angle)"""
+                image = pg.transform.rotate(self.images_color[self.color_switch[_i]%19], angle)
                 self.color_switch[_i] += 1
             screen.blit(image, image.get_rect(center=players[_i].position))
             if players[_i].freeze: screen.blit(self.image_freeze, players[_i].position+[-17, -50])
