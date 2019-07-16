8fd29f82b4a47d2b3749a3397f3f7dd3e2b230e5
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2019-07-16 14:38:29 +0800

    Create SetupAnacondaPrompt.bat

---
+++ SetupAnacondaPrompt.bat
@@ -0,0 +1,6 @@
+call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+pip install --upgrade pip
+pip uninstall -y numpy
+pip install --ignore-installed pygame numpy pillow moviepy
+
+pause
