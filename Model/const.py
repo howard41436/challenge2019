ae7db81474ab0fde7d316b8ec2963224b9264735
Chi-Feng Tsai <ktpss95112@yahoo.com.tw>
2019-07-16 15:24:03 +0800

    Update cut-in default player image

--- Model/const.py
+++ Model/const.py
@@ -1,6 +1,6 @@
 import View.const as view_const
 
-game_length = 60 * 60 * 5
+game_length = 60 * 60 * 4
 cutin_time = 90
 #dir const
 """
@@ -96,7 +96,7 @@ default_equipments = [
 priced_market_positions = [
     (376, 373.5)
 ]
-market_radius = 70
+market_radius = 50
 market_cd_time = 60 * 5
 
 item_price = {
