7699bfcf487c5953199c4077dc51d46152bded5f
Chi-Feng Tsai <b07902123@ntu.edu.tw>
2019-07-17 19:58:46 +0800

    Update SetupAnacondaPrompt.bat

--- SetupAnacondaPrompt.bat
+++ SetupAnacondaPrompt.bat
@@ -2,4 +2,7 @@ call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
 pip uninstall -y numpy
 pip install --ignore-installed pygame numpy pillow moviepy
 
+cd C:\Users\student\Downloads\challenge2019-master\challenge2019-master
+python main.py
+
 pause
